bb811c531d2a6331e70224f3e570c6057c8285e3
jih3508 <jih3508@ibizsoftware.net>
2023-03-06 17:35:08 +0900

    인풋 상태 관리

---
+++ 누구든지 하는 리액트 초심자를 위한 react 핵심 강좌/react-tutorials/contact-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PhoneForm from './components/PhoneForm';
+
+function App() {
+  return (
+    <div>
+      <PhoneForm />
+    </div>
+  );
+}
+
+export default App;
